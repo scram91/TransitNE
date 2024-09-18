f441a6b39ac616a0123ef90175382f8ddbe3b123
Sam Cramer <samuel.c.cramer@gmail.com>
2024-09-17 21:40:45 -0400

    Add User account Login and account register. Add unit test project and begin developing unit tests

--- TransitNE/Program.cs
+++ TransitNE/Program.cs
@@ -1,24 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using TransitNE.Data;
 using TransitNE.Models;
+using Microsoft.AspNetCore.Identity;
+using TransitNE.Data;
+using TransitNE.Areas.Identity.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TransitNEContextConnection") ?? throw new InvalidOperationException("Connection string 'TransitNEContextConnection' not found.");
 
-builder.Services.AddDbContext<TransitNEContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("TransitNEContext") ?? throw new InvalidOperationException("Connection string 'TransitNEContext' not found.")));
+builder.Services.AddDbContext<TransitNEContext>(options => options.UseSqlServer(connectionString));
 
-// Add services to the container.
+builder.Services.AddDefaultIdentity<TransitNEUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<TransitNEContext>();
+
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-
-    SeedData.Initialize(services);
 }
 
 // Configure the HTTP request pipeline.
@@ -33,11 +35,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
