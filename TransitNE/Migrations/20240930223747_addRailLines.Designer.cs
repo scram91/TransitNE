2afae400c6eef8482c17934908d0c09365479dd4
Sam Cramer <samuel.c.cramer@gmail.com>
2024-09-30 20:47:49 -0400

    Database updates

--- TransitNE/Migrations/20240930223747_addRailLines.Designer.cs
+++ TransitNE/Migrations/20240930223747_addRailLines.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TransitNE.Data;
 
@@ -11,9 +12,11 @@ using TransitNE.Data;
 namespace TransitNE.Migrations
 {
     [DbContext(typeof(TransitNEContext))]
-    partial class TransitNEContextModelSnapshot : ModelSnapshot
+    [Migration("20240930223747_addRailLines")]
+    partial class addRailLines
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -277,22 +280,6 @@ namespace TransitNE.Migrations
                     b.HasKey("ID");
 
                     b.ToTable("RailScheduleModels");
-                });
-
-            modelBuilder.Entity("TransitNE.Models.SeptaRailLines", b =>
-                {
-                    b.Property<int>("ID")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ID"));
-
-                    b.Property<string>("RailLine")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("ID");
-
-                    b.ToTable("SeptaRailLinesModels");
                 });
 
             modelBuilder.Entity("TransitNE.Models.TrainModel", b =>
---
+++ TransitNE/Migrations/20240930223955_addseptalines.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransitNE.Migrations
+{
+    /// <inheritdoc />
+    public partial class addseptalines : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SeptaRailLinesModels",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RailLine = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeptaRailLinesModels", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SeptaRailLinesModels");
+        }
+    }
+}
