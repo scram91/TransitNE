13a875723577c38a6dbf59bf0b8283e21a66727b
Sam Cramer <samuel.c.cramer@gmail.com>
2024-11-15 18:16:48 -0500

    Add map controller and view files Add Google Map to view

---
+++ TransitNE/Controllers/MapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransitNE.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Map()
+        {
+            return View();
+        }
+    }
+}
