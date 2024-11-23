252e449cd730677668056ce6972818324a090842
Sam Cramer <samuel.c.cramer@gmail.com>
2024-11-23 17:14:49 -0500

    Update address searching

---
+++ TransitNE/Models/AddressModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TransitNE.Models
+{
+    public class AddressModel
+    {
+        public string? StreetAddress { get; set; }
+        public string? Lat { get; set; }
+        public string? Long { get; set; }
+    }
+}
