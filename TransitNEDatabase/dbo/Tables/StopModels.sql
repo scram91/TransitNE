15746c63b324e2a1c26a68f1448a0510ceaf44a6
Samuel Cramer <67920223+scram91@users.noreply.github.com>
2024-12-03 19:18:34 -0500

    Delete TransitNEDatabase directory

--- TransitNEDatabase/dbo/Tables/StopModels.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[StopModels] (
-    [Id]       INT            IDENTITY (1, 1) NOT NULL,
-    [Lng]      NVARCHAR (MAX) NULL,
-    [Lat]      NVARCHAR (MAX) NULL,
-    [StopId]   NVARCHAR (MAX) NULL,
-    [StopName] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_StopModels] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
