6fc8055d9e705e5c917d2b891c73f2228417894d
Sam Cramer <samuel.c.cramer@gmail.com>
2024-12-07 18:01:27 -0500

    updates

--- TransitNEDatabase/dbo/Tables/BusTrolleyRouteModels.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[BusTrolleyRouteModels] (
-    [ID]          INT            IDENTITY (1, 1) NOT NULL,
-    [RouteNumber] NVARCHAR (MAX) NULL,
-    [RouteName]   NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_BusTrolleyRouteModels] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
